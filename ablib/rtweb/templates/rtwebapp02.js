a61f685cc1c476e61e5a7513fe9ca23f94c53699
ngm <ngm@ngmlab.(none)>
2014-03-04 09:41:49 -0500

    ...

--- ablib/rtweb/templates/rtwebapp02.js
+++ ablib/rtweb/templates/rtwebapp02.js
@@ -1,53 +1,57 @@
 ///////////////////////////////////////////////////////////////////////
-	function open_websocket(){
-		function show_message(message){
-			// var el = document.createElement('div');
-			// el.innerHTML = message;
-			// document.body.appendChild(el);
-			$("#json_res").html($("#json_res").text() + message + '\n');
-					//$('#scratchpad').text(data);
-					var psconsole = $('#json_res');
-					psconsole.scrollTop(psconsole[0].scrollHeight - psconsole.height());
+function open_websocket() {
+	function show_message(message) {
+		// var el = document.createElement('div');
+		// el.innerHTML = message;
+		// document.body.appendChild(el);
+		$("#json_res").html($("#json_res").text() + message + '\n');
+		//$('#scratchpad').text(data);
+		var psconsole = $('#json_res');
+		psconsole.scrollTop(psconsole[0].scrollHeight - psconsole.height());
 
-		}
-
-		var ws = new WebSocket("ws://127.0.0.1:8888/track");
-		ws.onopen = function() {
-			$('#live').val('ON').slider("refresh");
-			show_message('Connected.');
-		};
-		ws.onmessage = function(event) {
-	     
-	     var json_data;
-	        			try{
-	        				json_data = JSON.parse(event.data);	        				
-	        				console.log('json_data');
-	        				$('#slider1').val(json_data).slider("refresh");
-	        			}
-	        			catch(e){
-	        					console.log('JSON.parse error: "' + e +'"');
-	        				}
-
-	     
-	     // console.log(point[1][1] + point[1][2]);
-	     show_message(event.data);
-	 };
-	 ws.onclose = function() {
-	 	$('#live').val('OFF').slider("refresh");
-	 	show_message("Closed.");
-	 };
 	}
 
+	var ws = new WebSocket("ws://127.0.0.1:8888/track");
+	ws.onopen = function() {
+		$('#live').val('ON').slider("refresh");
+		show_message('Connected.');
+	};
+	ws.onmessage = function(event) {
+
+		var JsonData;
+		try {
+			JsonData = JSON.parse(event.data);
+			if (JsonData.hasOwnProperty('id'))			
+			{
+				console.log(JsonData.id);
+				$('#'+JsonData.id).val(JsonData.val).slider("refresh");
+			}
+			
+		} catch(e) {
+			
+			console.log('JSON.parse error: "' + e + '". JsonData = ' + JsonData);
+		}
+
+		// console.log(point[1][1] + point[1][2]);
+		show_message(event.data);
+	};
+	ws.onclose = function() {
+		$('#live').val('OFF').slider("refresh");
+		show_message("Closed.");
+	};
+}
+
+
 $(document).ready(function() {
-	console.log('Document ready');	
+	console.log('Document ready');
 	$('#json_res').attr('style', 'background-color:White; font-size:14px; height: 20em;');
-	
+
 	open_websocket();
 
 	///////////////////////////////////////////////////////////////////////
 	$('#json_cmd').keydown(function(e) {
 		if (e.keyCode == 13) {
-			var cmd = $("#json_cmd").val();			 			
+			var cmd = $("#json_cmd").val();
 			$(this).val("");
 			// $("#json_res").text("");
 
@@ -55,11 +59,10 @@ $(document).ready(function() {
 				console.log('Clear screen');
 				$("#json_res").text("");
 			} else {
-				if (cmd == ''){
+				if (cmd == '') {
 					console.log('Sending empty command');
 					cmd = ' ';
-				}
-				else{
+				} else {
 					console.log('Sending command: ' + cmd);
 				}
 
@@ -79,22 +82,20 @@ $(document).ready(function() {
 	///////////////////////////////////////////////////////////////////////
 	$("#console_clear").click(function() {
 		console.log('Pressed clear console button');
-		$("#cmd_status").text("");				
+		$("#cmd_status").text("");
 	});
-	
+
 	$("#options_threading").click(function() {
 		console.log('Pressed options_threading button');
-		SendCmd('options_threading',0)
+		SendCmd('options_threading', 0)
 		$("#cmd_status").text("Pressed options_ping button");
 	});
-	
-	
+
 	$("#options_ping").click(function() {
 		console.log('Pressed options_ping button');
-		SendCmd('ping',0)
+		SendCmd('ping', 0)
 		$("#cmd_status").text("Pressed options_ping button");
 	});
-	
 
 	function SendCmd(cmd, val) {
 		return $.getJSON('/cmd/', "cmd=" + cmd + "&param=" + val, function(data) {
@@ -103,5 +104,4 @@ $(document).ready(function() {
 		});
 	}
 
-
-}); 
+});
