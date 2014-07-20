a3843d0bd76718c4feda71ae4ada511586f1836a
andrzej <andrzej@borowiec.ca>
2014-07-20 14:12:10 -0500

    ...

--- ablib/daq/daq_digitemp.py
+++ ablib/daq/daq_digitemp.py
@@ -1,9 +1,6 @@
-from datastore import submit
-from redisbridge import Client
+import digitemp as dt
+from submit import submit
 
-
-C = Client(host='192.168.1.133',channel='dt')
-C.timeout = 30;
 
 done = False
 while not done:
@@ -11,6 +8,30 @@ while not done:
 	if out[0] == 0:
 		try:
 			data_set = [y[1:] for y in out[1]]
-			submit(out[1],submit_to="192.168.1.133",port=8000)
+			submit(out[1],submit_to="192.168.1.10",port=8000, threshold=0.5)
 		except:
 			pass
+
+def StartIqrSubmit(channel, host, submit_to):
+    print"StartIqrSubmit(%s, %s, %s)" % (channel, host, submit_to)
+    try:
+
+        while True:
+            pass
+    except KeyboardInterrupt:
+        pass
+    print "Exiting " + __name__
+
+##########################################################################################
+if __name__ == "__main__":
+    arguments = docopt(__doc__, version='Naval Fate 2.0')
+    print("===============================================")
+    print(arguments)
+
+    dev = arguments['--dev']
+
+    if arguments['run']:
+        channel   = 'rtweb'
+        host      = get_host_ip()
+        submit_to = arguments.get('--submit_to', get_host_ip())
+        StartIqrSubmit(channel, host, submit_to)
