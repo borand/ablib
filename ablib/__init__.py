9c0b776bd3253dcfe3902582fb7054bbf034ee09
Borand <borand@borowiec.ca>
2014-07-22 08:16:36 -0400

    ...

---
+++ ablib/__init__.py
@@ -0,0 +1,4 @@
+import analysis
+import daq
+import hardware
+import util
---
+++ alias.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+alias clc='clear'
+alias ve='source ~/venv/bin/activate'
+alias de='deactivate'
+alias ip='ipython'
+alias gitcom='git commit -am"..."'
