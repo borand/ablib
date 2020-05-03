e3e152210dd0738a6fe50dea33dd4315e5213609
Dr. Zej. B <drzejb@borowiec.ca>
2020-05-02 20:17:23 -0400

    updated requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@
 # based on: https://github.com/kennethreitz/setup.py
 
 from setuptools import setup, find_packages
+import os
 
+the_lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = the_lib_folder + '/requirements.txt'
+install_requires = [] # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
 
 with open('README.md') as f:
     readme = f.read()
@@ -20,5 +27,6 @@ setup(
     author_email='borand@borowiec.ca',
     url='https://github.com/borand/ablib',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=install_requires
 )
