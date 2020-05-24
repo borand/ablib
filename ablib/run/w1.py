46dfb0c2a06e4ff34fb53adcb7a08d00e5de9508
Dr. Zej B <borand@borowiec.ca>
2020-05-23 22:48:35 -0400

    0523

--- ablib/run/w1.py
+++ ablib/run/w1.py
@@ -1,7 +1,17 @@
 import logging
 from ablib.sensors import onewire
 
+try:
+    import systemd.daemon
+    systemd_loaded = True
+except:
+    systemd_loaded = False
+    pass
+
 
 def start(args):
     logging.info(f'Starting 1-wire acquisition, args: {args}')
+    if systemd_loaded:
+        systemd.daemon.notify('READY=1')
+
     onewire.run()
