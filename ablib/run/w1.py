eec81803e9a228bab7de0505bdea608163121bfc
Dr. Zej B <borand@borowiec.ca>
2020-05-23 22:54:59 -0400

    0523

--- ablib/run/w1.py
+++ ablib/run/w1.py
@@ -12,6 +12,9 @@ except:
 def start(args):
     logging.info(f'Starting 1-wire acquisition, args: {args}')
     if systemd_loaded:
-        systemd.daemon.notify('READY=1')
+        try:
+            systemd.daemon.notify('READY=1')
+        except:
+            pass
 
     onewire.run()
